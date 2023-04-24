bc21c4f5941a76bd4eeec2f5e4f5ded9fbe00e06
Ostap Chichiginarov <ostap00034@gmail.com>
2023-04-24 21:14:03 +0900

    added base registration, login, recieving tokens

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { PrismaService } from './prisma.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, PrismaService],
+	imports: [ConfigModule.forRoot(), AuthModule],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,115 @@
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { AuthDto } from './dto/auth.dto'
+import { hash, verify } from 'argon2'
+import { User } from '@prisma/client'
+import { JwtService } from '@nestjs/jwt'
+import { RefreshTokenDto } from './dto/refresh-token.dto'
+
+@Injectable()
+export class AuthService {
+	constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto)
+		const tokens = await this.issueTokens(user.id)
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		}
+	}
+
+	async getNewTokens(dto: RefreshTokenDto) {
+		const result = await this.jwt.verifyAsync(dto.refreshToken)
+		if (!result) throw new UnauthorizedException('Неверный refresh token')
+
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id: result.id,
+			},
+		})
+
+		const tokens = await this.issueTokens(user.id)
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		}
+	}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.prisma.user.findUnique({
+			where: {
+				contacts: {
+					email: dto.email,
+					phone: dto.phone,
+				},
+			},
+		})
+
+		if (oldUser) throw new BadRequestException('Пользователь уже существует.')
+
+		const user = await this.prisma.user.create({
+			data: {
+				email: dto.email,
+				password: await hash(dto.password),
+				fio: dto.fio,
+				phone: dto.phone,
+				role: dto.role,
+			},
+		})
+
+		const tokens = await this.issueTokens(user.id)
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		}
+	}
+
+	private async issueTokens(userId: number) {
+		const data = { id: userId }
+
+		const accessToken = this.jwt.sign(data, {
+			expiresIn: '1h',
+		})
+
+		const refreshToken = this.jwt.sign(data, {
+			expiresIn: '6d',
+		})
+
+		return { accessToken, refreshToken }
+	}
+
+	private returnUserFields(user: User) {
+		return {
+			id: user.id,
+			email: user.email,
+		}
+	}
+
+	private async validateUser(dto: AuthDto) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				contacts: {
+					email: dto.email,
+					phone: dto.phone,
+				},
+			},
+		})
+
+		if (!user) throw new NotFoundException('Пользователь не найден.')
+
+		const isValid = await verify(user.password, dto.password)
+
+		if (!isValid) throw new UnauthorizedException('Неверный пароль.')
+
+		return user
+	}
+}
